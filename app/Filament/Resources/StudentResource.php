d4604a72f980cf88bc8551fae4ee63c9a3805114
gustifa <fauzangustifa@gmail.com>
2024-08-12 16:59:27 +0700

    Belajar Filament

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StudentResource\Pages;
+use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+
+use Filament\Tables\Columns\TextColumn;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->autofocus()
+                    ->required(),
+                TextInput::make('email')
+                    ->email(),
+                Select::make('class_id')
+                    ->relationship(name: 'class', titleAttribute: 'name'),
+                Select::make('section_id')
+                    ->relationship(name: 'section', titleAttribute: 'name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Nama')->sortable()->searchable(),
+                TextColumn::make('class.name')->label('Nama Kelas')->sortable()->searchable(),
+                TextColumn::make('section.name')->sortable()->searchable(),
+                TextColumn::make('email')->label('E-mail')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Section.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'class_id',
+        'name',
+
+    ];
+
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+}
